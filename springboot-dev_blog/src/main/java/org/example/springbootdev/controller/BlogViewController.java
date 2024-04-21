ce3523c2cca3f1d7ae613163c29c7089be117150
taein111 <taein4546@naver.com>
2024-04-21 17:11:44 +0900

    블로그 뷰 페이지 구현

--- springboot-dev_blog/src/main/java/org/example/springbootdev/controller/BlogViewController.java
+++ springboot-dev_blog/src/main/java/org/example/springbootdev/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -33,5 +34,16 @@ public class BlogViewController {
         Article article = blogservice.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model){ //필수가 아닌 파라미터로 설정하기 위해 required = false
+        if (id==null) { //id 가 없으면 생성.
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {//id 있으면 수정
+            Article article = blogservice.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
